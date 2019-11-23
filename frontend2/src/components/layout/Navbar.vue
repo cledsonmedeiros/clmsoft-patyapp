63339dbf162174d96923b23d95e87a95820b6e3d
Cledson Medeiros <cledson.lmedeiros@hotmail.com>
2019-11-23 12:57:23 -0300

    add vuetify

---
+++ frontend2/src/components/layout/Navbar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <v-app-bar app dense color="primary" dark>
+      <div class="d-flex align-center">
+        <v-img alt="Vuetify Logo" class="shrink mr-2" contain src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png" transition="scale-transition" width="40" />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank" text>
+        <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      
+    }
+  },
+}
+</script>
