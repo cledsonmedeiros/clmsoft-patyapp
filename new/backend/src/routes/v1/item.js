e7f6627c8c5f17e24ccd6f64beca68428a29fb49
Cledson Medeiros <cledson.lmedeiros@hotmail.com>
2020-01-29 16:54:03 -0300

    revisão produtos

--- new/backend/src/routes/v1/item.js
+++ new/backend/src/routes/v1/item.js
@@ -2,7 +2,8 @@ const routes = require('express').Router();
 const ItemController = require('../../controllers/ItemController');
 
 routes.get('/', ItemController.index);
-routes.get('/venda/:venda', ItemController.getByVenda);
+routes.get('/venda/:id', ItemController.getByVenda);
+routes.get('/produto/:id', ItemController.getByProduto);
 routes.get('/:id', ItemController.show);
 routes.post('/', ItemController.store);
 routes.put('/:id', ItemController.update);
